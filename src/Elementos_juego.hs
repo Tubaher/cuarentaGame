497820ae6758889785528f576bd1050c64b8ecca
Tubaher <38712907+Tubaher@users.noreply.github.com>
2018-11-19 01:00:59 -0500

    solving the build problem

--- src/Elementos_juego.hs
+++ src/Elementos_juego.hs
@@ -9,22 +9,22 @@ type Baraja = [Carta]
 {-creandoCarta ::Integer -> String -> Carta
 creandoCarta x y = Carta(x,y) -}
 
-creandoCarta :: [String]->[Integer] -> Carta
+{- creandoCarta :: [String]->[Integer] -> Carta
 creandoCarta xs yx = [Carta | (x <- xs, y <- ys, let x=(x,y))]
+ -}
 
-
-isInBaraja :: Carta -> Baraja -> Bool
+{- isInBaraja :: Carta -> Baraja -> Bool
 isInBaraja carta bara = filter (isOrNot) bara 
             where isOrNot x  
                         | carta == x = True
-                        | carta \= x = False
+                        | carta /= x = False -}
                         
 --addBaraja :: Carta -> [Carta] -> [Carta]
 --addBaraja carta x = carta : x
 --addBaraja (x:xs)
 
-addBaraja :: Carta -> Baraja -> Baraja
-addBaraja x baraja = takeWhile((length baraja) <= 53) filter(isInBaraja x)baraja
+{- addBaraja :: Carta -> Baraja -> Baraja
+addBaraja x baraja = takeWhile((length baraja) <= 53) filter(isInBaraja x)baraja -}
 
 data Mano = Mano { carta1 :: Carta
                  , carta2 :: Carta
