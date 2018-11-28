d4b5fb7983d5dcf01050abd206f775d0e4cb9134
fapbricio <jonnathan.crespo@yachaytech.edu.ec>
2018-11-28 02:20:24 -0500

    Agregando comentarios a la funciones de Mecanicas_juego

--- src/Mecanicas_juego.hs
+++ src/Mecanicas_juego.hs
@@ -4,29 +4,52 @@ import Elementos_juego
 import qualified Data.Maybe as Maybe
 import qualified Data.List as List 
 
+----------------- listaIndices-------------------
+--Funcion que me permite obtener todos los indices de cada Carta de la Mano y almacenarlos en una lista
+
 
 listaIndices :: Mano -> [Integer]
 listaIndices (Mano{ carta1 = a, carta2 = b, carta3 = c, carta4 =  d, carta5 = e}) = map snd [a,b,c,d,e]
+
+----------------- verificarTriunfoInicial-------------------
+--Funcion que me permite verificar si tengo 4 cartas que tengan el mismo indice, si es asi, me devuelve True, en otro caso False
+
 
 verificarTriunfoInicial :: [Integer] -> Bool
 verificarTriunfoInicial xs  
     | True `List.elem` map (\ws -> (length ws) ==4) (List.group (List.sort xs)) = True
     | otherwise = False
      
+
+----------------- verificarRonda-------------------
+--Funcion que me permite verificar si tengo 3 cartas con los mismos indices, si es asi, me devuelve True, en otro caso False
+
+
 verificarRonda :: [Integer] -> Bool
 verificarRonda xs  
     | True `List.elem` map (\ws -> (length ws) ==3) (List.group (List.sort xs)) = True
     | otherwise = False
-     
+
+----------------- ultimoIndice-------------------
+--Funcion que me permite obtener el indice de la ultima carta que ingresa a la mesa
+
 ultimoIndice :: Mesa -> Integer
 ultimoIndice l = foldl (\acc (_,numero) -> if acc>numero then acc else numero) 0 l
- 
+
+----------------- ultimoIndiceElem-------------------
+--Funcion que me devuelve la carta que corresponde al ultimo indice
+
 ultimoIndiceElem :: Mesa -> Carta
 ultimoIndiceElem mesa = fst(Maybe.fromMaybe (("brillo",1),-1) (List.find (\(_, indice) -> indice == ultimoIndice mesa) mesa ))
-    
+
+----------------- botarCartaMesa-------------------   
 --Esta función recibe una carta y la agrega a la mesa con un contador para tener en cuenta en que orden entró
+
 botarCartaMesa :: Carta -> Mesa -> Mesa 
 botarCartaMesa c m = (c, ultimoIndice(m)+1) : m 
-     
+
+----------------- botarCarta-------------------
+--Funcion que me permite botar una carta en Game
+
 botarCarta :: Carta -> Game -> Game 
 botarCarta cart gm =  gm
